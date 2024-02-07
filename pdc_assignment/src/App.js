f6fb08ab1fe322dfa39e8050cb64525efe890966
chetan791 <chetanchauhan795@gmail.com>
2024-02-07 16:13:30 +0530

    completed all things

---
+++ pdc_assignment/src/App.js
@@ -0,0 +1,79 @@
+import "./App.css";
+import styled from "styled-components";
+import ReactIcon from "./assets/react.png";
+import apple from "./assets/apple.png";
+import github from "./assets/github.png";
+import googleplay from "./assets/google-play.png";
+import { Authentication } from "./components/Authentication";
+
+function App() {
+  return (
+    <DIV>
+      <h1 id="MainHeader">Test Assignment</h1>
+      <p id="intro">
+        Hello, My name is Chetan I specialize in MERN Stack as following
+        shown,and this is my test submission,
+      </p>
+      <div id="icons">
+        <img
+          style={{ border: "3px solid #a7de95" }}
+          src={ReactIcon}
+          alt="React"
+        />
+        <img style={{ border: "3px solid #cd98ce" }} src={github} alt="React" />
+        <img style={{ border: "3px solid #b85a4b" }} src={apple} alt="React" />
+        <img
+          style={{ border: "3px solid #57768d" }}
+          src={googleplay}
+          alt="React"
+        />
+      </div>
+      <Authentication />
+    </DIV>
+  );
+}
+
+export default App;
+
+const DIV = styled.div`
+  margin: 0;
+  padding: 0;
+  height: 100px;
+  #MainHeader {
+    text-align: center;
+    background-color: #d3d3d3;
+    padding: 20px;
+    font-weight: 400;
+    width: 100%;
+  }
+  #intro {
+    text-align: center;
+    padding: 20px;
+    background-color: #f6eaea;
+    border: 3px solid #d9e77d;
+    border-radius: 15px;
+    margin: 10px 30px;
+  }
+  #icons {
+    display: flex;
+    justify-content: space-evenly;
+    padding: 20px;
+  }
+  img {
+    height: 100px;
+    border: 3px solid black;
+    padding: 30px;
+    border-radius: 25px;
+    box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
+  }
+  @media (max-width: 600px) {
+    #icons {
+      display: grid;
+      grid-template-columns: 1fr 1fr;
+      grid-gap: 10px;
+      grid-template-rows: 1fr 1fr;
+      border: 1px solid black;
+      padding-left: 11%;
+    }
+  }
+`;
---
+++ pdc_assignment/src/components/Authentication.jsx
@@ -0,0 +1,152 @@
+import React, { useState } from "react";
+import styled from "styled-components";
+
+export const Authentication = () => {
+  const [showLogin, setShowLogin] = useState(false);
+  const [Register, setRegister] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+  const [Login, setLogin] = useState({
+    email: "",
+    password: "",
+  });
+
+  const [token, setToken] = useState(null);
+
+  const handleLogin = async () => {
+    try {
+      const res = await fetch(
+        "https://pdc-assignment-backend.onrender.com/pdc/login",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(Login),
+        }
+      );
+      const data = await res.json();
+      setToken(data.token);
+      console.log(data.token);
+      if (data.token) {
+        alert("logged in");
+      }
+    } catch (error) {
+      console.log(error);
+      alert("Error in login");
+    }
+  };
+
+  const handleRegister = async () => {
+    try {
+      const res = await fetch(
+        "https://pdc-assignment-backend.onrender.com/pdc/register",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(Register),
+        }
+      );
+      const data = await res.json();
+      console.log(data);
+      alert(data);
+    } catch (error) {
+      console.log(error);
+      alert("Error in register");
+    }
+  };
+
+  return (
+    <DIV>
+      <button
+        style={{ marginBottom: "10px", marginLeft: "45%" }}
+        onClick={() => setShowLogin(!showLogin)}
+      >
+        {showLogin ? "Go to Login" : "Go to Register"}
+      </button>
+      {!token && showLogin ? (
+        <div id="register">
+          <h3>Register</h3>
+          <input
+            type="text"
+            placeholder="Name"
+            onChange={(e) => setRegister({ ...Register, name: e.target.value })}
+          />
+          <input
+            type="email"
+            placeholder="Email"
+            onChange={(e) =>
+              setRegister({ ...Register, email: e.target.value })
+            }
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            onChange={(e) =>
+              setRegister({ ...Register, password: e.target.value })
+            }
+          />
+          <button onClick={handleRegister}>Register</button>
+        </div>
+      ) : (
+        <div id="login">
+          <h3>Login</h3>
+          <input
+            type="email"
+            placeholder="Email"
+            onChange={(e) => setLogin({ ...Login, email: e.target.value })}
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            onChange={(e) => setLogin({ ...Login, password: e.target.value })}
+          />
+          <button onClick={handleLogin}>Login</button>
+        </div>
+      )}
+
+      {token && (
+        <div
+          style={{
+            backgroundColor: "green",
+            borderRadius: "10px",
+            textAlign: "center",
+          }}
+        >
+          Logged in
+        </div>
+      )}
+    </DIV>
+  );
+};
+
+const DIV = styled.div`
+  div {
+    display: flex;
+    flex-direction: column;
+    width: 400px;
+    padding: 10px;
+    margin: auto;
+    gap: 10px;
+    font-size: large;
+  }
+  input {
+    padding: 10px;
+    font-size: large;
+  }
+  button {
+    margin: auto;
+    font-size: large;
+    padding: 10px;
+    border-radius: 10px;
+    border: 2px solid black;
+  }
+  button:hover {
+    background-color: black;
+    color: white;
+  }
+`;
